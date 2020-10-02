3ccb33198598c8ef587c90efce8196727b85fbd6
Lee Eunha <lee95625@gmail.com>
2020-10-02 16:20:36 +0900

    조건 각 항목 select, input 추가

--- src/components/Condition.vue
+++ src/components/Condition.vue
@@ -1,14 +1,45 @@
 <template>
   <div class="Condition">
-    {{ $t(`stats.${condition.statId}`) }} - {{ $t(`condition.${condition.type}`) }} - {{ condition.value }}
+    <AppSelect class="Condition__element" name="id" :value="conditionForm.statId" :options="statIds" @change="updateId" />
+    <AppInput class="Condition__element" type="text" :value="conditionForm.value" @input="updateValue" />
+    <AppSelect class="Condition__element" name="type" :value="conditionForm.type" :options="conditionTypes" @change="updateType" />
   </div>
 </template>
 
 <script>
+import { statIdList } from '@/stats';
+
 export default {
   name: 'Condition',
   props: {
     condition: { type: Object, required: true }
+  },
+  data() {
+    return {
+      conditionForm: { ...this.condition }
+    };
+  },
+  methods: {
+    updateId(statId) {
+      this.conditionForm = { ...this.conditionForm, statId };
+    },
+    updateType(type) {
+      this.conditionForm = { ...this.conditionForm, type };
+    },
+    updateValue(value) {
+      this.conditionForm = { ...this.conditionForm, value };
+    }
+  },
+  computed: {
+    conditionTypes() {
+      return ['gte', 'lt'].map(type => ({ value: type, label: this.$t(`condition.${type}`) }));
+    },
+    statIds() {
+      return statIdList.map(id => ({
+        value: id,
+        label: this.$t(`stats.${id}`)
+      }));
+    }
   }
 };
 </script>
@@ -17,5 +48,11 @@ export default {
 .Condition {
   height: 34px;
   margin-top: 10px;
+}
+
+.Condition__element {
+  & + & {
+    margin-left: 10px;
+  }
 }
 </style>
