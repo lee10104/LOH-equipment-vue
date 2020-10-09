a1cea8b5b91660a7eb0e7b317e20674b76a9df1f
Lee Eunha <lee95625@gmail.com>
2020-10-09 15:06:25 +0900

    주의사항 추가

--- src/components/NoticeDialog.vue
+++ src/components/NoticeDialog.vue
@@ -1,10 +1,45 @@
 <template>
   <AppDialog @close="$emit('close')">
-    <template #title></template>
-    <template #content></template>
+    <template #title>{{ $t('notice.title') }}</template>
+    <template #content>
+      <div class="NoticeDialog__content">
+        <div class="NoticeDialog__content-title">
+          {{ $t('notice.precaution.title') }}
+        </div>
+        <div
+          v-html="$t('notice.precaution.description')"
+          class="NoticeDialog__content-description"
+        />
+      </div>
+      <div class="NoticeDialog__content">
+        <div class="NoticeDialog__content-title">
+          {{ $t('notice.instruction.title') }}
+        </div>
+        <div
+          v-html="$t('notice.instruction.description')"
+          class="NoticeDialog__content-description"
+        />
+      </div>
+    </template>
   </AppDialog>
 </template>
 
 <script>
 export default { name: 'NoticeDialog' };
 </script>
+
+<style lang="scss">
+.NoticeDialog__content {
+  & + & {
+    margin-top: 10px;
+  }
+}
+
+.NoticeDialog__content-title {
+  font-weight: 700;
+}
+
+.NoticeDialog__content-description {
+  margin-top: 10px;
+}
+</style>
